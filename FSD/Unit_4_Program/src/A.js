df821754b664936405db6050906df8955f42fed2
Jeel Patel <6201jeel15patel@gmail.com>
2023-06-09 12:39:10 +0530

    Change

--- FSD/Unit_4_Program/src/A.js
+++ FSD/Unit_4_Program/src/A.js
@@ -3,17 +3,17 @@ const app = expr();
 const path = require("path");
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({extended:false}))
-const staticPath = path.join(__dirname, "../public")
+app.use(bodyParser.urlencoded({ extended: false }));
+const staticPath = path.join(__dirname, "../public");
 
-app.get('/',(req,res)=>{
-   res.sendFile(staticPath + "/X.html")
-})
+app.get("/", (req, res) => {
+  res.sendFile(staticPath + "/X.html");
+});
 
-app.post("/process",(req,res)=>{
-    res.send(req.body)
-})
+app.post("/process", (req, res) => {
+  res.send(req.body);
+});
 
 app.listen(9999, () => {
-    console.log("Server Running");
+  console.log("Server Running");
 });
