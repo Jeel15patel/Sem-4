478ac65767c57a10e6a3fd20c38b1fb92ff84efc
Jeel Patel <6201jeel15patel@gmail.com>
2023-06-09 12:10:53 +0530

    Update(9/6/23)

---
+++ FSD/Unit_4_Program/src/form2.js
@@ -0,0 +1,19 @@
+// 
+
+
+const expr = require("express");
+const app = expr();
+const path = require("path");
+
+const staticPath = path.join(__dirname, "../public");
+app.use(expr.static(staticPath,{index:"form.html"}));
+
+app.get('/process',(req,res)=>{
+    username = req.query.username
+    password = req.query.password
+    res.send(`<h1>Username is ${username} and your password is ${password}`)
+})
+
+app.listen(9999, () => {
+    console.log("Server Running");
+});
