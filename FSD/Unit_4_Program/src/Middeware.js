16b6e1df710d38e408ae9b8e76ad10719151c0de
Jeel Patel <6201jeel15patel@gmail.com>
2023-06-12 10:19:46 +0530

    Lect - 12/6/23

--- FSD/Unit_4_Program/src/Middeware.js
+++ FSD/Unit_4_Program/src/Middeware.js
@@ -24,24 +24,24 @@ const port = 9009;
 
 // -----------------------------------------------
 
-// app.use("/",(req, res, next) => {
-//         res.set("Content-Type", "text/html");
-//         res.write("Request Received on " + new Date());
-//         next();
-//     },
-//     (req, res, next) => {
-//         res.write("<h1><center>Hello World</center></h1>");
-//         next();
-//     },
-//     (req, res) => {
-//         res.write("<h1><center>WELCOME</center></h1>");
-//         res.send()
-//     }
-// );
+app.use("/",(req, res, next) => {
+        res.set("Content-Type", "text/html");
+        res.write("Request Received on " + new Date());
+        next();
+    },
+    (req, res, next) => {
+        res.write("<h1><center>Hello World</center></h1>");
+        next();
+    },
+    (req, res) => {
+        res.write("<h1><center>WELCOME</center></h1>");
+        res.send()
+    }
+);
   
-// app.listen(port, () => {
-//   console.log("Server is running on port " + port);
-// });
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
   
 // -----------------------------------------------
 
---
+++ FSD/Unit_4_Program/src/Pra_2.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,"Pra_2.html"))
+})
+
+app.post("/check",(req, res, next) => {
+    if (req.body.username == "admin") {
+      next();
+    } else {
+      res.write("<h1> You are not admin </h1>");
+      res.send()
+    }
+});
+
+app.post("/check",(req, res, next) => {
+    var data = req.body.username
+    res.write("<h1> You are admin </h1>"+data);
+    res.send()
+})
+
+app.listen(9999, () => {
+  console.log("Server Running");
+});
