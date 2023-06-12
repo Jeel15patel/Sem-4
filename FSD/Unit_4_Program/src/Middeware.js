151bae885283a79404ab7df8c01af78a0ea36475
Jeel Patel <6201jeel15patel@gmail.com>
2023-06-12 09:36:55 +0530

    12/6/23 - fsd

--- FSD/Unit_4_Program/src/Middeware.js
+++ FSD/Unit_4_Program/src/Middeware.js
@@ -24,23 +24,41 @@ const port = 9009;
 
 // -----------------------------------------------
 
-app.use("/",(req, res, next) => {
-        res.set("Content-Type", "text/html");
-        res.write("Request Received on " + new Date());
-        next();
-    },
-    (req, res, next) => {
-        res.write("<h1><center>Hello World</center></h1>");
-        next();
-    },
-    (req, res) => {
-        res.write("<h1><center>WELCOME</center></h1>");
-        res.send()
-    }
-);
+// app.use("/",(req, res, next) => {
+//         res.set("Content-Type", "text/html");
+//         res.write("Request Received on " + new Date());
+//         next();
+//     },
+//     (req, res, next) => {
+//         res.write("<h1><center>Hello World</center></h1>");
+//         next();
+//     },
+//     (req, res) => {
+//         res.write("<h1><center>WELCOME</center></h1>");
+//         res.send()
+//     }
+// );
   
-app.listen(port, () => {
-  console.log("Server is running on port " + port);
-});
+// app.listen(port, () => {
+//   console.log("Server is running on port " + port);
+// });
   
 // -----------------------------------------------
+
+// const reqFilter = (req,res,next) => {
+//     console.log("Thank")
+//     res.write("Hello")
+//     next()
+// }
+// app.use(reqFilter)
+// app.get("/",(req,res)=>{
+//     res.write("Hello 2")
+//     res.send()
+// })
+
+// app.listen(port, () => {
+//   console.log("Server is running on port " + port);
+// });
+
+// -----------------------------------------------
+
