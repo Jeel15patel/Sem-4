2515b156091e5f982796f381d1d74d70a553c024
Jeel Patel <6201jeel15patel@gmail.com>
2023-06-13 08:50:37 +0530

    Cookie - 12/6

--- FSD/Unit_4_Program/src/Cookie.js
+++ FSD/Unit_4_Program/src/Cookie.js
@@ -1,5 +1,31 @@
-// import cookle-parser module
-
 var cp = require("cookie-parsen")
+const expr = require("express");
+const app = expr();
 
-res.cookie("name","value","expiry")
+// app.use(cp())
+
+// app.get("/",(req,res,next)=>{
+//     res.cookie("Uname","ABC",{expires:new Data(Data.new()+20000)})
+//     next()
+// })
+
+// app.get("/",(req,res,next)=>{
+//     console.log(res.cookie)
+//     next()
+//     res.send("HELLOW")
+// })
+
+
+app.use(cp())
+app.get('/', (req, res, next) => {
+    res.cookie(('username','xyz',{expires:new Date(Date.now()+20000)}))
+    next();
+}, (req, res, next) => {
+    console.log(req.cookies)
+    next();
+    res.send("Hello")
+})
+
+app.listen(9999, () => {
+    console.log("Server Running");
+  });
