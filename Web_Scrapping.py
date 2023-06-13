bdca20617bbe0ad463b7c7a58a5275dae95c9cc4
Jeel Patel <6201jeel15patel@gmail.com>
2023-06-13 10:50:13 +0530

    13/06/23

---
+++ Web_Scrapping.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+website = "https://subslikescript.com/movie/Titanic-120338"
+
+result = requests.get(website)
+content = result.text
+soup = BeautifulSoup(content, "html.parser")
+# print(soup)
+print(soup.prettify())
+
+
+
+movie = soup.find("article",class_="main-article")
+print(movie)
