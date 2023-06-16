3a3ddc692a57b6cb83e8510f38dab4f72f89b9b2
Jeel Patel <6201jeel15patel@gmail.com>
2023-06-16 12:30:38 +0530

    Session

--- FSD/Unit_4_Program/src/Cookies_2.js
+++ FSD/Unit_4_Program/src/Cookies_2.js
@@ -2,41 +2,25 @@
 // it should jump to the next page and value of username should be stored in cookie.
 // Perform task using http get method, observer and check cookie inside the browser.
 
-// const express = require('express');
-// const path = require('path');
-// const cp = require('cookie-parser');
-// const app = express()
-// app.use(express.static(path.join(__dirname), { index: 'Cookies_2.html' }))
-// app.use(cp())
-// app.get('/task4Process', (req, res) => {
-//     data = req.query.username
-//     res.cookie(('username', data, { expires: new Date(Date.now() + 20000) })) 
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
 
-//     console.log(req.cookies)
-// })
-// app.listen(9999)
+app.use(cookieParser());
 
-var cp=require('cookie-parser');
-const express=require("express");
-const app=express();
-var bodyParser=require("body-parser");
-var path=require("path");
-app.use(cp());
-
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/cookies_2.html");
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/cookies_2.html");
 });
 
-app.get("/task4Process",(req,res,next)=>{
-    const responses={username:req.query.uname}
-    res.cookie("username",responses.username)
-    
-    next();
+app.get("/task4Process", (req, res, next) => {
+  const responses = { username: req.query.uname };
+  res.cookie("username", responses.username);
+  next();
 });
 
-app.get("/task4Process",(req,res)=>{
-    res.send(req.cookies);
-    console.log(res.cookie)
+app.get("/task4Process", (req, res) => {
+  console.log(req.cookies); // Log cookies to the console
+  res.send(req.cookies);
 });
 
 app.listen(9009, () => {
