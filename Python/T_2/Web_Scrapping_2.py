1ca44ac47844fb53cf62777d8188feb04f40533f
Jeel Patel <6201jeel15patel@gmail.com>
2023-06-17 12:04:35 +0530

    PY 17-6-23

--- Python/T_2/Web_Scrapping_2.py
+++ Python/T_2/Web_Scrapping_2.py
@@ -1,21 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-website = "https://subslikescript.com/movies"
+website = "https://subslikescript.com/movies_letter-Y"
 root = "https://subslikescript.com/"
 
 result = requests.get(website)
 content = result.text
 soup = BeautifulSoup(content, "html.parser")
-
-# print(soup.prettify())
-
-# print(soup.find_all("a", href=True))
-
-# print(box.find_all("a",href=True))
-
-# print(len(box.find_all("a",href=True)))
-
 box = soup.find("article", class_="main-article")
 print(box)
 
@@ -27,20 +18,22 @@ for link in box.find_all("a", href=True):
     links.append(link["href"])
 print(links)
 
-print("---------------B-2-------------------")
+links = links[1::]
 
-for link in links:
-    url = root + link
+for i in links:
+    url = root + i
     print(url)
     result = requests.get(url)
     content = result.text
     soup = BeautifulSoup(content, "html.parser")
     transcript = soup.find("div", class_="full-script").text.strip()
-    title = soup.find("h1").text.split()
+    title = soup.find("h1").text.strip()
     try:
-        with open(f"{title}1.txt", "w", encoding="utf-8") as f:
+        with open(
+            f"D:\Github\Sem-4\Python\T_2\Script\{title.replace('?','')}1.txt", "w", encoding="utf-8"
+        ) as f:
             f.write(transcript)
     except Exception as e:
         print(e)
 
-print("---------------C-3--------------------")
+print("---------------B-2-------------------")
