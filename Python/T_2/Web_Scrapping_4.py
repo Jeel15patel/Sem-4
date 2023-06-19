55dfe194f564042c81f0a50479be6216a8f033de
Jeel Patel <6201jeel15patel@gmail.com>
2023-06-19 17:12:06 +0530

    PY Update

--- Python/T_2/Web_Scrapping_4.py
+++ Python/T_2/Web_Scrapping_4.py
@@ -7,7 +7,7 @@ import pandas as pd
 website = "https://www.imdb.com/search/title/?release_date=2018-01-01,2018-12-31"
 
 result = requests.get(website)
-content = result.text   
+content = result.text
 soup = BeautifulSoup(content, "html.parser")
 
 nameYearList = soup.find_all("h3", class_="lister-item-header")
@@ -21,6 +21,6 @@ for i in range(len(nameYearList)):
         nameYearList[i].find("span", class_="lister-item-year").text.strip()
     )
     data["Rating"].append(ratingList[i].find("strong").text.strip())
-    
+
 df = pd.DataFrame(data)
 print(df)
--- Python/T_2/Web_Scrapping_5.py
+++ Python/T_2/Web_Scrapping_5.py
@@ -1,10 +1,24 @@
-import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
 soup = BeautifulSoup(
-    open("Python\\Web Scrapping\\top250.html", "r", encoding="utf-8"), "html.parser"
+    open(
+        "Web Scrapping\\top250.html",
+        "r",
+        encoding="utf-8",
+    ),
+    "html.parser",
 )
-print(soup.prettify())
+nameYearList = soup.find_all("td", class_="titleColumn")
+ratingList = soup.find_all("td", class_="imdbRating")
 
-movies = soup.findAll('div',class_:'')
+data = {"Title": [], "Year": [], "Rating": []}
+
+for i in range(len(nameYearList)):
+    data["Title"].append(nameYearList[i].find("a").text.strip())
+    data["Year"].append(nameYearList[i].find("span").text.strip()[1:-1])
+    data["Rating"].append(ratingList[i].find("strong").text.strip())
+
+df = pd.DataFrame(data)
+df.to_csv("WS3.csv")
+print(df)
