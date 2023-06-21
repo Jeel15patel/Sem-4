6caf94600eecc4c7c09ff3e97559f2f068d110d2
Jeel Patel <6201jeel15patel@gmail.com>
2023-06-21 12:10:21 +0530

    PUG First Pra

--- FSD/Unit_5_Program/Pug_1.js
+++ FSD/Unit_5_Program/Pug_1.js
@@ -1,12 +1,12 @@
 var expr = require("express")
 var app = expr()
-app.set("view engine","png")
+app.set("view engine","pug")
 var p = require("path")
 
-const staticPath = p.join(__dirname,"../public")
+const staticPath = p.join(__dirname)
 
 app.get("/",(req,res) =>{
-    res.render(staticPath+"/one")
+    res.render(staticPath+"/Pug_1")
 })
 
 app.listen(9999, () => {
