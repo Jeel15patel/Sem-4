025b32e2ab880c663b634df01f094928acac2ebe
Jeel Patel <6201jeel15patel@gmail.com>
2023-07-17 10:11:45 +0530

    Lect 1 Install React

--- FSD/Unit_6_Program_T3/myapp1/src/App.js
+++ FSD/Unit_6_Program_T3/myapp1/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Hello friend This is my first App</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello This is My First React App</h1>
+        <br></br>
+        <table>
+          <tr>
+            <th>Lectern No.</th>
+            <th>Date</th>
+            <th>Contain</th>
+          </tr>
+          <tr>
+            <td>Lec 1</td>
+            <td>17-07-23</td>
+            <td>Install React</td>
+          </tr>
+        </table>
       </header>
     </div>
   );
