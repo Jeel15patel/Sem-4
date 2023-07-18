96048ccdb4b1ad3001a473f2a497944c6a564e0a
Jeel Patel <6201jeel15patel@gmail.com>
2023-07-18 10:32:04 +0530

    Edit

--- Python/T_3/Unit_6/RPS_Game.py
+++ Python/T_3/Unit_6/RPS_Game.py
@@ -62,12 +62,20 @@ def play_again():
 
 def play_rock_paper_scissors(rounds=3):
     global user_wins, computer_wins, ties_wins
+    user_wins = 0 ; computer_wins = 0 ; ties_wins = 0
     for round_games in range(1, rounds + 1):
         print(f"\nRound {round_games}:")
         user_choice = get_user_choice()
         computer_choice = get_computer_choice()
         get_rock_paper_scissors_result(computer_choice, user_choice)
-
+        
+    if user_wins > computer_wins:
+        print("Congratulations! You are the overall winner!")
+    elif user_wins < computer_wins:
+        print("Computer is the overall winner!")
+    else:
+        print("It's a tie!")
+        
     answer = play_again()
     if answer == "Y":
         play_rock_paper_scissors()
