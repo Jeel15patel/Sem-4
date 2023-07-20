66e2b6a7b216ab22ab6d23e032dec166401be4df
Jeel Patel <6201jeel15patel@gmail.com>
2023-07-20 08:56:38 +0530

    RSPLS Games

--- Python/T_3/Unit_6/RPS_Game.py
+++ Python/T_3/Unit_6/RPS_Game.py
@@ -3,81 +3,73 @@
 
 import random
 
-user_wins = 0
-computer_wins = 0
-ties_wins = 0
-
 
 def get_user_choice():
-    global user_wins, computer_wins, ties_wins
-    print("User Wins:", user_wins)
-    print("Computer Wins:", computer_wins)
-    print("Ties:", ties_wins)
-    print("Enter R for Rock, P for Paper, S for Scissors")
-    user_choice = input("Your choice: ").strip().upper()
-    while user_choice not in ("R", "P", "S"):
+    while True:
+        user_choice = (
+            input("Enter R for Rock, P for Paper, S for Scissors: ").strip().upper()
+        )
+        if user_choice in ("R", "P", "S"):
+            return user_choice
         print("I don't understand that!")
-        user_choice = input("Your choice: ").strip().upper()
-    return user_choice
 
 
 def get_computer_choice():
-    computer_choice_number = random.randrange(3)
-    if computer_choice_number == 0:
-        computer_choice = "R"
-    elif computer_choice_number == 1:
-        computer_choice = "P"
-    else:
-        computer_choice = "S"
-    return computer_choice
+    choices = {"R": "Rock", "P": "Paper", "S": "Scissors"}
+    return random.choice(list(choices.keys()))
 
 
 def get_rock_paper_scissors_result(computer_choice, user_choice):
-    global user_wins, computer_wins, ties_wins
-    print("Computer's choice:", computer_choice)
+    print(f"Computer's choice: {computer_choice}")
     if computer_choice == user_choice:
         print("Tie!")
-        ties_wins += 1
-    elif (
+        return "Tie"
+    if (
         (computer_choice == "R" and user_choice == "S")
         or (computer_choice == "S" and user_choice == "P")
         or (computer_choice == "P" and user_choice == "R")
     ):
         print("Computer wins!")
-        computer_wins += 1
-    else:
-        print("You win!")
-        user_wins += 1
+        return "Computer"
+    print("You win!")
+    return "User"
 
 
 def play_again():
-    print(f"Score - You: {user_wins}, Computer: {computer_wins}, Ties: {ties_wins}")
-    print("Do you want to play again?")
-    answer = input("Y for yes, N for no: ").strip().upper()
-    while answer not in ("Y", "N"):
-        print("I don't understand that!")
-        answer = input("Y for yes, N for no: ").strip().upper()
-    return answer
+    return (
+        input("Do you want to play again? (Y for yes, N for no): ").strip().upper()
+        == "Y"
+    )
 
 
 def play_rock_paper_scissors(rounds=3):
-    global user_wins, computer_wins, ties_wins
-    user_wins = 0 ; computer_wins = 0 ; ties_wins = 0
+    user_wins = 0
+    computer_wins = 0
+    ties_wins = 0
+
     for round_games in range(1, rounds + 1):
         print(f"\nRound {round_games}:")
         user_choice = get_user_choice()
         computer_choice = get_computer_choice()
-        get_rock_paper_scissors_result(computer_choice, user_choice)
-        
+        result = get_rock_paper_scissors_result(computer_choice, user_choice)
+
+        if result == "User":
+            user_wins += 1
+        elif result == "Computer":
+            computer_wins += 1
+        else:
+            ties_wins += 1
+
+    print(f"Score - You: {user_wins}, Computer: {computer_wins}, Ties: {ties_wins}")
+
     if user_wins > computer_wins:
         print("Congratulations! You are the overall winner!")
     elif user_wins < computer_wins:
         print("Computer is the overall winner!")
     else:
         print("It's a tie!")
-        
-    answer = play_again()
-    if answer == "Y":
+
+    if play_again():
         play_rock_paper_scissors()
     else:
         print("Thanks for playing!")
--- Python/T_3/Unit_6/RSPLS_Game.py
+++ Python/T_3/Unit_6/RSPLS_Game.py
@@ -1,1 +1,80 @@
-# Rock, Spock, Paper, Lizard, Scissors
+# Rock,Paper,Scissors,Lizard,Spock Games
+
+import random
+
+def get_user_choice():
+    while True:
+        user_choice = (
+            input(
+                "Enter R for Rock, P for Paper, S for Scissors, L for Lizard, V for Spock: "
+            )
+            .strip()
+            .upper()
+        )
+        if user_choice in ("R", "P", "S", "L", "V"):
+            return user_choice
+        print("Invalid choice. Please try again.")
+
+
+def get_computer_choice():
+    choices = ["R", "P", "S", "L", "V"]
+    return random.choice(choices)
+
+
+def get_rock_paper_scissors_result(computer_choice, user_choice):
+    choices = ["R", "P", "S", "L", "V"]
+    difference = (choices.index(computer_choice) - choices.index(user_choice)) % 5
+
+    if difference == 1 or difference == 2:
+        return "Computer"
+    elif difference == 3 or difference == 4:
+        return "User"
+    else:
+        return "Tie"
+
+
+def play_again():
+    return (
+        input("Do you want to play again? (Y for yes, N for no): ").strip().upper()
+        == "Y"
+    )
+
+
+def play_rock_paper_scissors(rounds=3):
+    user_wins = 0
+    computer_wins = 0
+    ties_wins = 0
+
+    for round_games in range(1, rounds + 1):
+        print(f"\nRound {round_games}:")
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        result = get_rock_paper_scissors_result(computer_choice, user_choice)
+
+        if result == "User":
+            user_wins += 1
+            print("You win!")
+        elif result == "Computer":
+            computer_wins += 1
+            print("Computer wins!")
+        else:
+            ties_wins += 1
+            print("Tie!")
+
+    print(f"Score - You: {user_wins}, Computer: {computer_wins}, Ties: {ties_wins}")
+    
+    if user_wins > computer_wins:
+        print("Congratulations! You are the overall winner!")
+    elif user_wins < computer_wins:
+        print("Computer is the overall winner!")
+    else:
+        print("It's a tie!")
+
+    if play_again():
+        play_rock_paper_scissors()
+    else:
+        print("Thanks for playing!")
+
+
+
+play_rock_paper_scissors()
