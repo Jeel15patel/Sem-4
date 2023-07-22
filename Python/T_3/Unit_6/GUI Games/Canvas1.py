4d9cd4c9c5c8139fcce4961bb9a91192056d9ea0
Jeel Patel <6201jeel15patel@gmail.com>
2023-07-22 10:12:26 +0530

    Lect 3

--- Python/T_3/Unit_6/GUI Games/Canvas1.py
+++ Python/T_3/Unit_6/GUI Games/Canvas1.py
@@ -9,6 +9,7 @@ frame.set_canvas_background :- It use for background color.
 canvas.draw_line :- It use for draw line.
 canvas.draw_polyline :- It use for draw line with multiple points.
 canvas.draw_polygon :- It use for draw polygon with multiple points. ( It connect the last point to first point)
+canvas.draw_circle :- It use for draw circle with center point and radius.
 """
 # --------------------------------------------------------
 
@@ -35,7 +36,7 @@ canvas.draw_polygon :- It use for draw polygon with multiple points. ( It connec
 # frame.start()
 
 # --------------------------------------------------------
-
+# draw_line
 
 # def draw_handler(canvas):
 #     canvas.draw_line((10, 20), (60, 60), 12, "Red")
@@ -47,25 +48,46 @@ canvas.draw_polygon :- It use for draw polygon with multiple points. ( It connec
 # frame.start()
 
 # --------------------------------------------------------
-
+# draw_polyline
 
 # def draw_handler(canvas):
 #     canvas.draw_polyline([(50, 100), (42, 446), (678, 800), (200, 250)], 5, "Red")
 
 
-# frame = simple.create_frame("ConntectLine Frame", 500, 500, 200)
+# frame = simple.create_frame("Polyline Frame", 500, 500, 200)
 # frame.set_draw_handler(draw_handler)
 # frame.start()
 
 # --------------------------------------------------------
-
+# draw_polygon
 
 # def draw_handler(canvas):
 #     canvas.draw_polygon([(50, 100), (42, 446), (346, 467), (200, 250)], 5, "Red", "Green")
 
 
-# frame = simple.create_frame("ConntectLine Frame", 500, 500, 200)
+# frame = simple.create_frame("Polygon Frame", 500, 500, 200)
 # frame.set_draw_handler(draw_handler)
 # frame.start()
 
 # --------------------------------------------------------
+# draw_circle
+
+# def draw_handler(canvas):
+#     canvas.draw_circle((100, 100), 50, 2, "Red", "yellow")
+#     canvas.draw_circle((250, 250), 200, 5, "Red", "yellow")  
+
+
+# frame = simple.create_frame("Circle Frame", 500, 500, 200)
+# frame.set_draw_handler(draw_handler)
+# frame.start()
+
+# --------------------------------------------------------
+# Frame with lable
+
+# frame = simple.create_frame("ConntectLine Frame", 500, 500, 200)
+# lable1 = frame.add_label("Hello")
+# lable2 = frame.add_label("World",100)
+# frame.start()
+
+# --------------------------------------------------------
+
