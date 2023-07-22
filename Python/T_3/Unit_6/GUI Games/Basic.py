20922ca32c4c6a76fb38ba2add574504db2c465b
Jeel Patel <6201jeel15patel@gmail.com>
2023-07-22 09:06:37 +0530

    Lect 4 py

--- Python/T_3/Unit_6/GUI Games/Basic.py
+++ Python/T_3/Unit_6/GUI Games/Basic.py
@@ -1,7 +1,39 @@
-import SimpleGUICS2Pygame as simp
+import SimpleGUICS2Pygame.simpleguics2pygame as simple
+
+
+# def timer_handler():
+#     print("Timer Handler called")
+#     # timer.stop() # stop timer after 1 tick
+
+
+# timer = simple.create_timer(1000, timer_handler)
+# timer.start()
+
+
+# ---------------------------------------------
+# Print onle 5 Times 
+
+# t = 0
+# def timer_handler():
+#     global t
+#     while t == 5:
+#         timer.stop()
+#     print(t,"Hello")
+#     t = t + 1    
+
+# timer = simple.create_timer(1000, timer_handler)
+# timer.start()
+
+# ---------------------------------------------
 
 def timer_handler():
-    print("Timer")
+    pass
 
-timer = simp.create_timer(500, timer_handler)
-timer.start()
+timers = simple.create_timer(500, timer_handler)
+print(timers.is_running())
+
+timers.start()
+print(timers.is_running())
+
+timers.stop()
+print(timers.is_running())
