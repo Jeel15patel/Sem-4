4d9e471a099dabf4e318dce11341113bc2f2ed96
Jeel Patel <6201jeel15patel@gmail.com>
2023-07-22 09:32:11 +0530

    Lect 3 PY

--- Python/T_3/Unit_6/GUI Games/Canvas1.py
+++ Python/T_3/Unit_6/GUI Games/Canvas1.py
@@ -1,8 +1,22 @@
 import SimpleGUICS2Pygame.simpleguics2pygame as simple
 
+mycoloes = ["Red", "Green", "Blue", "Yellow"]
+t = 0
+
+
+def timer_handler():
+    global t
+    t += 1
+    if t == 4:
+        t = 0
+
+
 def draw_handler(canvas):
-    frame.set_canvas_background("Red")
-    
-frame = simple.create_frame("First Frame", 200, 200,100)
-frame.set_draw_handler("Hello",draw_handler)
+    frame.set_canvas_background(mycoloes[t])
+
+
+timers = simple.create_timer(500, timer_handler)
+frame = simple.create_frame("First Frame", 200, 200, 100)
+frame.set_draw_handler(draw_handler)
+timers.start()
 frame.start()
