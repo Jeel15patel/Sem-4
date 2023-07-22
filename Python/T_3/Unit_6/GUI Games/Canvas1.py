af8d2d93d182a4108e4f5f87e6c1d4bc94fd72e5
Jeel Patel <6201jeel15patel@gmail.com>
2023-07-22 09:45:42 +0530

    Lect 3

--- Python/T_3/Unit_6/GUI Games/Canvas1.py
+++ Python/T_3/Unit_6/GUI Games/Canvas1.py
@@ -1,5 +1,14 @@
 import SimpleGUICS2Pygame.simpleguics2pygame as simple
 
+# --------------------------------------------------------
+
+"""The code is creating a simple graphical user interface (GUI) using the SimpleGUICS2Pygame library inPython.
+The code is creating a frame with a title of “My Frame” and a size of 200 pixels by 200 pixels.
+draw_text :- It use for Font size, Font color, Font style, Font position.
+set_canvas_background :- It use for background color.
+"""
+# --------------------------------------------------------
+
 mycoloes = ["Red", "Green", "Blue", "Yellow"]
 t = 0
 
@@ -13,6 +22,7 @@ def timer_handler():
 
 def draw_handler(canvas):
     frame.set_canvas_background(mycoloes[t])
+    canvas.draw_text(mycoloes[t], (50, 50), 50, "Black")
 
 
 timers = simple.create_timer(500, timer_handler)
