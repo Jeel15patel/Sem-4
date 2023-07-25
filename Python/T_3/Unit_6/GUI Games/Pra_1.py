828126010c8abc416bdbf4cf0c48057dc5aebefd
Jeel Patel <6201jeel15patel@gmail.com>
2023-07-25 09:54:41 +0530

    Pra_1 PY Lec 4

--- Python/T_3/Unit_6/GUI Games/Pra_1.py
+++ Python/T_3/Unit_6/GUI Games/Pra_1.py
@@ -1,6 +1,5 @@
 import SimpleGUICS2Pygame.simpleguics2pygame as simple
 
-
 def draw_handler(canvas):
     canvas.draw_polyline([(50, 50), (450, 450)], 5, "Red")
     canvas.draw_polyline([(450, 50), (50, 450)], 5, "Red")
@@ -15,3 +14,13 @@ def draw_handler(canvas):
 frame = simple.create_frame("Pra_1", 500, 500, 200)
 frame.set_draw_handler(draw_handler)
 frame.start()
+
+
+"""
+The above code creates a simple GUI window using the SimpleGUICS2Pygame library and draws a red X
+shape with yellow circles at the corners and a white square in the center.
+
+:param canvas: The canvas is the area where you can draw shapes and objects. In this code, the
+canvas is created using the create_frame() function from the SimpleGUICS2Pygame library. The canvas
+size is set to 500x500 pixels
+"""
