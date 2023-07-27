ad1edcfa6fbd8b6d909f1c37ecfaaba337636469
Jeel Patel <6201jeel15patel@gmail.com>
2023-07-27 08:48:22 +0530

    Change PY

--- Python/T_3/Unit_6/GUI Games/Pra_1.py
+++ Python/T_3/Unit_6/GUI Games/Pra_1.py
@@ -1,4 +1,14 @@
+"""
+The above code creates a simple GUI window using the SimpleGUICS2Pygame library and draws a red X
+shape with yellow circles at the corners and a white square in the center.
+
+:param canvas: The canvas is the area where you can draw shapes and objects. In this code, the
+canvas is created using the create_frame() function from the SimpleGUICS2Pygame library. The canvas
+size is set to 500x500 pixels
+"""
+
 import SimpleGUICS2Pygame.simpleguics2pygame as simple
+
 
 def draw_handler(canvas):
     canvas.draw_polyline([(50, 50), (450, 450)], 5, "Red")
@@ -9,18 +19,11 @@ def draw_handler(canvas):
     canvas.draw_circle((450, 50), 50, 5, "yellow")
     canvas.draw_circle((50, 450), 50, 5, "yellow")
 
-    canvas.draw_polygon([[150,150],[350,150],[350,350],[150,350]],5,'white','white')
+    canvas.draw_polygon(
+        [[150, 150], [350, 150], [350, 350], [150, 350]], 5, "white", "white"
+    )
+
 
 frame = simple.create_frame("Pra_1", 500, 500, 200)
 frame.set_draw_handler(draw_handler)
 frame.start()
-
-
-"""
-The above code creates a simple GUI window using the SimpleGUICS2Pygame library and draws a red X
-shape with yellow circles at the corners and a white square in the center.
-
-:param canvas: The canvas is the area where you can draw shapes and objects. In this code, the
-canvas is created using the create_frame() function from the SimpleGUICS2Pygame library. The canvas
-size is set to 500x500 pixels
-"""
--- Python/T_3/Unit_6/GUI Games/Pra_2.py
+++ Python/T_3/Unit_6/GUI Games/Pra_2.py
@@ -1,8 +1,18 @@
+"""
+This is a simple calculator program that takes two numbers as input and performs addition,
+subtraction, multiplication, and division operations on them.
+
+:param inp: The parameter "inp" is used as the input value for the number entered by the user. It is
+passed to the input handler functions "InputHandlerNum1" and "InputHandlerNum2" when the user enters
+a number in the input fields
+"""
+
 import SimpleGUICS2Pygame.simpleguics2pygame as simple
 
 num1 = 0
 num2 = 0
 ans = 0
+
 
 def InputHandlerNum1(inp):
     global num1
@@ -35,7 +45,7 @@ def div():
 
 
 def draw_handler(canvas):
-    canvas.draw_text(str(ans), (250, 250), 50,"Red")
+    canvas.draw_text(str(ans), (250, 250), 50, "Red")
 
 
 frames = simple.create_frame("Calculator Pra_2", 500, 500, 200)
