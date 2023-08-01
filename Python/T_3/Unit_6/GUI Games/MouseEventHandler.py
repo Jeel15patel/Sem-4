1aa6b796903b78253b4d02deba803d04e973349d
Jeel Patel <6201jeel15patel@gmail.com>
2023-08-01 14:39:02 +0530

    Lect 6 PY

--- Python/T_3/Unit_6/GUI Games/MouseEventHandler.py
+++ Python/T_3/Unit_6/GUI Games/MouseEventHandler.py
@@ -4,5 +4,6 @@ def click(pos):
     print(pos)
     
 frames = Simplegui.create_frame("Mouse Click", 300, 300)
-frames.set_mouseclick_handler(click)
+# frames.set_mouseclick_handler(click)
+frames.set_mousedrag_handler(click)
 frames.start()
