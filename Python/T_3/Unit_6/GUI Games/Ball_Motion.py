2aa455ff3dff86de62c053fd2b415574200b05f8
Jeel Patel <6201jeel15patel@gmail.com>
2023-08-01 10:44:43 +0530

    Lect 6 PY

--- Python/T_3/Unit_6/GUI Games/Ball_Motion.py
+++ Python/T_3/Unit_6/GUI Games/Ball_Motion.py
@@ -7,14 +7,19 @@ width = 500
 height = 500
 radiu = 30
 position = [int(width / 2), int(height / 2)]
-velocity = [0,1]
+# velocity = [1, 0] # right
+# velocity = [0, 1] # down
+# velocity = [-1, 0] # left
+velocity = [0, -1]  # up
+
 
 def draw(canvas):
     global position, velocity
     position[0] += velocity[0]
     position[1] += velocity[1]
     canvas.draw_circle(position, radiu, 5, "red", "white")
-    
+
+
 frame = simple.create_frame("Ball Motion", width, height)
 frame.set_draw_handler(draw)
 frame.start()
