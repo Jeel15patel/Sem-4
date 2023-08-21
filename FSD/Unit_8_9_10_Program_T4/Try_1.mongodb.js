aa6dc44e8e99214dacaac97c68d7b1b4f22bfe1e
Jeel Patel <6201jeel15patel@gmail.com>
2023-08-21 22:18:12 +0530

    T4_FSD

---
+++ FSD/Unit_8_9_10_Program_T4/Try_1.mongodb.js
@@ -0,0 +1,15 @@
+use("Try_1")
+
+
+// db.try_1.insertMany([
+//     {"Name": "Sachin", "Age": 20, "City": "Mumbai"},
+//     {"Name": "Rahul", "Age": 37, "City": "Surat"},
+//     {"Name": "Saurav", "Age": 44, "City": "Ahmedabad"}
+// ])
+
+
+// db.try_1.updateOne({"Name": "Sachin"}, {$set: {"City": "Pune"}})
+
+// db.try_1.deleteOne({"Name": "Saurav"})
+
+db.try_1.find()
