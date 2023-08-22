406c0034e2e8654ded0537e2b8d745baa922ac0b
Jeel Patel <6201jeel15patel@gmail.com>
2023-08-22 10:28:05 +0530

    T4_PY

--- Python/T_4/myproject/myapp/urls.py
+++ Python/T_4/myproject/myapp/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, Home!")
+
+def room(request):
+    return HttpResponse("Hello, Room!")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("myapp.urls")),
+    path('', home),
+    path('room/', room),
 ]
