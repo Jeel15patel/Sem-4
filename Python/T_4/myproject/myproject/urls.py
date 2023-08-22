46549789bb9f29cc449560910ff05dbb13252fb8
Jeel Patel <6201jeel15patel@gmail.com>
2023-08-22 10:17:11 +0530

    PY

--- Python/T_4/myproject/myproject/urls.py
+++ Python/T_4/myproject/myproject/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello, Home!")
+
+def room(request):
+    return HttpResponse("Hello, Room!")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('room/', room),
 ]
