8b83f6044f170a40954cdfeaef18549639957832
Jeel Patel <6201jeel15patel@gmail.com>
2023-08-22 21:49:42 +0530

    T4 FSD 21-08-2023 Lect:1

---
+++ FSD/Unit_8_9_10_Program_T4/LJ.mongodb.js
@@ -0,0 +1,37 @@
+use("LJ");
+
+// db.employee.insertOne({
+//   E_Name: "Jeel",
+//   Age: 20,
+//   Phone: 1234567890,
+//   Designation: "Developer",
+// });
+
+// db.employee.insertMany([
+//   { "E_Name": "Dwij", "Age": 22, "Phone": 1230984567, "Designation": "Developer" },
+//   { "E_Name": "Harsh", "Age": 24, "Phone": 5672348906, "Designation": "Developer" },
+//   { "E_Name": "Happy", "Age": 25, "Phone": 1029384756, "Designation": "Developer" },
+//   { "E_Name": "N1", "Age": 30, "Phone": 1357908642, "Designation": "Developer" },
+//   { "E_Name": "N1", "Age": 34, "Phone": 1256908743, "Designation": "Developer" },
+// ]);
+
+// db.employee.updateOne(
+//   { E_Name: "Dwij" },
+//   { $set: { Designation: "Senior Developer" } }
+// );
+
+
+// db.employee.updateOne(
+//   { E_Name: "Harsh" },
+//   { $set: { Designation: "Full Stack Developer" } }
+// );
+
+
+// db.employee.updateOne(
+//   { E_Name: "Jeel" },
+//   { $set: { Designation: "Hacker" } }
+// );
+
+// db.employee.find({ E_Name: "Jeel" });
+
+db.employee.find();
---
+++ FSD/Unit_8_9_10_Program_T4/mydb.mongodb.js
@@ -0,0 +1,22 @@
+use("mydb");
+
+// db.students.insertOne({
+//   name: "John",
+//   age: 23,
+// });
+
+// db.students.insertMany([
+//   { name: "Jeel", age: 20 },
+//   { name: "Dwij", age: 22 },
+//   { name: "Harsh", age: 24 },
+//   { name: "Happy", age: 25 },
+//   { name: "N1", age: 30 },
+//   { name: "N1", age: 34 },
+//   { name: "N1", age: 38 },
+// ]);
+
+// db.students.updateOne({ name: "John" }, { $set: { age: 25 } });
+
+// db.students.findOne({ name: "John" })
+
+db.students.find();
