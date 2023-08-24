d7b9b416e34bc0eef41b51f80b33d0b6e26e398a
Jeel Patel <6201jeel15patel@gmail.com>
2023-08-24 09:52:18 +0530

    T4 Py

--- Python/T_4/myproject/myapp/views.py
+++ Python/T_4/myproject/myapp/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 # Create your views here.
 
+homes = [
+    { "id": 1, "name": "Home 1"},
+    { "id": 2, "name": "Home 2"},
+    { "id": 3, "name": "Home 3"},
+]
+
+rooms = [
+    { "id": 1, "name": "Room 1"},
+    { "id": 2, "name": "Room 2"},
+    { "id": 3, "name": "Room 3"},
+]
+
 def home(request):
-    return HttpResponse("Hello, Home!")
+    return render(request, "home.html", {"homes": homes})
 
 def room(request):
-    return HttpResponse("Hello, Room!")
+    return render(request, "room.html", {"rooms": rooms})
