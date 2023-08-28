ff745a6070b418bbdf50dbcec9190cf61936f3e4
Jeel Patel <6201jeel15patel@gmail.com>
2023-08-28 09:51:31 +0530

    T4 FSD

---
+++ FSD/Unit_8_9_10_Program_T4/Mongoose/DataModel.js
@@ -0,0 +1,37 @@
+const mg = require("mongoose");
+
+mg.connect("mongodb://127.0.0.1:27017/LJ")
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const mySchema = new mg.Schema({
+  Name: {
+    type: String,
+    required: true,
+  },
+  Surname: String,
+  Age: Number,
+  Actve: Boolean,
+  Date: { type: Date, default: Date.now },
+});
+
+const person = new mg.model("Person", mySchema);
+
+const createDoc = async () => {
+  try {
+    const personData = new person({
+      Name: "Dwij",
+      Surname: "Patel",
+      Age: 22,
+      Actve: true,
+    });
+    const result = await personData.save();
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+createDoc();
