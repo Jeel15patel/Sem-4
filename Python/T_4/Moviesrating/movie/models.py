2ee04ec7fa803d90eeac7af559dc633e54298edf
Jeel Patel <6201jeel15patel@gmail.com>
2023-08-29 10:31:52 +0530

    T4 PY

--- Python/T_4/Moviesrating/movie/models.py
+++ Python/T_4/Moviesrating/movie/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+
 class Movies(models.Model):
     title = models.CharField(max_length=100)
     descp = models.CharField(max_length=250)
     image = models.ImageField(upload_to="movie/image")
     url = models.URLField(blank=True)
-    
+
     def __str__(self):
         return self.title
-    
--- Python/T_4/Moviesrating/movie/views.py
+++ Python/T_4/Moviesrating/movie/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movies
+
+
 # Create your views here.
 def home(request):
-    searchTerm=request.GET.get('searchMovie')
-    return render(request,'home.html',{'searchTerm':searchTerm})
+    searchTerm = request.GET.get("searchMovie")
+    if searchTerm:
+        movies = Movies.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movies.objects.all()
+    return render(request, "home.html", {"searchTerm": searchTerm, "movies": movies})
+
 
 def about(request):
-    return render(request,'about.html')
+    return render(request, "about.html")
+
 
 def signup(request):
-    email=request.GET.get('email')
-    return render(request,'signup.html',{'email':email})
+    email = request.GET.get("email")
+    return render(request, "signup.html", {"email": email})
